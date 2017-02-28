071aa9ba58559479808ef1990accd656d884a519
utkarsh <utkarshrai003@gmail.com>
2017-02-28 23:59:49 +0530

    Added message model

---
+++ api/models/Message.js
@@ -0,0 +1,29 @@
+/**
+ * Message.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    Id: {
+      type: "integer",
+      autoIncrement: true,
+      primaryKey: true
+    },
+    content: {
+      type: "string",
+      required: true
+    },
+    user: {
+      model: 'user'
+    },
+    room: {
+      model: 'room'
+    }
+
+  }
+};
